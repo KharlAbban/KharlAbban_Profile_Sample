17d46b5a3c02e09f252889aacc666599bb4ec972
Er-Carl Abban <102951384+KharlAbban@users.noreply.github.com>
2023-06-18 22:24:48 +0100

    Mod files

--- frontendmentor_advice_app/index.js
+++ frontendmentor_advice_app/index.js
@@ -1,18 +1,7 @@
-const adviceIdContainer = document.querySelector("[data-advice-id]");
-const adviceButton = document.querySelector("[data-advice-random]");
-const adviceContentContainer = document.querySelector("[data-advice-content]");
-getAdvice();
-adviceButton.addEventListener("click", () => {
-  getAdvice();
-});
-
-async function getAdvice () {
-  adviceContentContainer.textContent = "Advice Loading...";
-  adviceIdContainer.textContent = "";
-  const url = "https://api.adviceslip.com/advice";
-  const response = await fetch(url);
-  const toJson = await response.json();
-  console.log(toJson);
-  adviceIdContainer.textContent = toJson.slip.id;
-  adviceContentContainer.textContent = `"${toJson.slip.advice}"`;
-}
+const btn = document.querySelectorAll("[data-accordion-button]");
+btn.forEach((button, index) => {
+  button.addEventListener("click", (event) => {
+    button.classList.toggle("active");
+    button.parentElement.parentElement.querySelector("[data-accordion-content]").classList.toggle("open");
+  })
+})
